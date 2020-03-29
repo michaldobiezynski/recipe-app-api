f675a6ffe315d9ea1ea328bf82485c47fcdb0a3d
michaldobiezynski <dobiezynski@gmail.com>
2020-03-29 13:24:20 +0100

    writing a unit test with TDD

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,3 +3,8 @@
 def add(x, y):
     """Add two numbers together"""
     return x + y
+
+
+def substract(x, y):
+    """Substract x from y and return value"""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, substract
 
 
 class CalcTests(TestCase):
@@ -8,3 +8,7 @@ class CalcTests(TestCase):
     def test_Add_numbers(self):
         """Test that two numbers are added together"""
         self.assertEqual(add(3, 8), 11)
+
+    def test_substract_numbers(self):
+        """Test that values are substracted and returned"""
+        self.assertEqual(substract(5, 11), 6)
